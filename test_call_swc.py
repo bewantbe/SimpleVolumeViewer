7cf5e348a39b54553a6c38680e5279390c21c323
Eddy Xiao <bewantbe@gmail.com>
2023-01-11 14:58:13 +0800

    Add demo script.

---
+++ test_call_swc.py
@@ -0,0 +1,14 @@
+#
+
+import numpy as np
+import tifffile
+
+def PluginMain(ren, iren, guictrl):
+    swc_objs = guictrl.GetObjectsByType('swc')
+    print('Number of SWC:', len(swc_objs))
+#    for o in swc_objs:
+#        o.visible = np.random.rand() < 0.1
+    for o in swc_objs:
+        o.color = np.random.rand(3)
+    iren.GetRenderWindow().Render()
+    # TODO: make it possible to open a ipython environment, like Matlab.
